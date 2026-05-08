9d18eea67bc8819df3f3f5be0644e77846cff68a
Jesús Pedro Rodriguez <jesuspedro.rodriguez@unir.net>
2026-05-08 22:24:03 +0200

    fix: update base path in astro.config and use dynamic base URL for avatar in CV data

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jesusprodriguezUnir.github.io',
-  base: process.env.GITHUB_ACTIONS ? '/CV' : undefined,
+  base: process.env.GITHUB_ACTIONS ? '/CV/' : undefined,
 });
